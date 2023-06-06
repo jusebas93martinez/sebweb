ce80f65717c6e2672036d9d8569bcd79783a7299
Juan Sebastian Martinez Riveros <sebas93martinez@gmail.com>
2023-06-05 23:42:13 -0500

    primer commit

---
+++ poligonal/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+import pandas as pd
+import random
+import numpy as np
+import math
+import os
+from django.conf import settings
+from .utils import procesar_archivos
+import pandas as pd
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import matplotlib.ticker as ticker
+
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from .utils import procesar_archivos
+
+def error_404(request, exception):
+    return render(request, '404.html', status=404)
+
+def index(request):
+    return render(request, 'base.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def cargar_archivos(request):
+    if request.method == 'POST':
+        pol_file = request.FILES['pol_file']
+        bases_file = request.FILES['bases_file']
+
+        # Guardar los archivos en las rutas especificadas
+        with open('media/pol.csv', 'wb') as destination:
+            for chunk in pol_file.chunks():
+                destination.write(chunk)
+        
+        with open('media/bases.csv', 'wb') as destination:
+            for chunk in bases_file.chunks():
+                destination.write(chunk)
+
+        # Redirigir a la vista de procesamiento de archivos
+        return HttpResponseRedirect(reverse('procesar_archivos') + f'?pol_file={pol_file.name}&bases_file={bases_file.name}')
+
+
+    return render(request, 'loadfile.html')
+
+def procesar_archivos_view(request):
+    if request.method == 'GET':
+        pol_file = request.GET.get('pol_file')
+        bases_file = request.GET.get('bases_file')
+
+        datos = procesar_archivos()
+
+        # Generar el gráfico con Matplotlib
+        fig, ax = plt.subplots(figsize=(8, 6))
+        este_values = [resultado['Este'] for resultado in datos['resultados']]
+        norte_values = [resultado['Norte'] for resultado in datos['resultados']]
+        visados_values = [resultado['visado'] for resultado in datos['resultados']]
+        ax.plot(este_values + [este_values[0]], norte_values + [norte_values[0]], marker='o')
+
+
+        ax.set_xlabel('Este')
+        ax.set_ylabel('Norte')
+        ax.set_title('Poligonal')
+        ax.grid(True)
+
+        ax.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+        ax.yaxis.get_major_formatter().set_scientific(False)
+        ax.yaxis.get_major_formatter().set_useOffset(False)
+        
+        ax.xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+        ax.xaxis.get_major_formatter().set_scientific(False)
+        ax.xaxis.get_major_formatter().set_useOffset(False)
+
+
+
+        # Agregar etiquetas a los puntos
+        for x, y, label in zip(este_values, norte_values, visados_values):
+            ax.text(x, y, label, fontsize=8, ha='center', va='bottom')
+
+        # Guardar el gráfico en un archivo temporal
+        img_path = 'media/temporal.png'
+        plt.savefig(img_path)
+        plt.close()
+
+        # Agregar la ruta de la imagen al diccionario de datos
+        datos['img_path'] = img_path
+
+        random_value = random.randint(1, 100000)  # Generar un valor aleatorio
+        datos['random_value'] = random_value
+        datos['img_path'] = f'{img_path}?{random_value}'
+
+        return render(request, 'resultados.html', datos)
---
+++ sebweb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sebweb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sebweb.settings')
+
+application = get_asgi_application()
