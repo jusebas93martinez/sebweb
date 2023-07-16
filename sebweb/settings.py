416567b9ce4fe3922ce151832114e286f00c2884
jusebas93martinez <sebas93martinez@gmail.com>
2023-07-15 23:05:28 -0500

    c2

--- sebweb/settings.py
+++ sebweb/settings.py
@@ -71,11 +71,8 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'sebweb.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
